74d664f3c0ec1f851652e10995f6ef46944166dc
SubhashSVS <svs.subhash2003@gmail.com>
2024-08-25 02:11:35 +0530

    fixed the navigation

--- frontend/src/components/NavigationText.jsx
+++ frontend/src/components/NavigationText.jsx
@@ -1,10 +1,12 @@
+import {Link} from 'react-router-dom';
+
 const NavigationText = ({context,route})=>{
     return <div className="mt-3 text-center">
         {context == "signup" ?
             "Already have an account? "
             : "Don't have an account? "
         }
-        <a href={context == "signup" ? "/signin" : "/signup"} className="font-medium"><u>{route}</u></a>
+        <Link to={context === "signup" ? "/signin" : "/signup"} className="font-medium"><u>{route}</u></Link>
     </div>
 }
 export default NavigationText;
